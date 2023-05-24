e1a088e2eb35e43d71b968a2a0e7743c56fa4eb9
Renzo <91288365+Renzo-Henrique@users.noreply.github.com>
2023-05-24 10:40:54 -0300

    transformar comprehension para map

--- Trab02/T2_Renzo-Henrique-Guzzo_Leao.py
+++ Trab02/T2_Renzo-Henrique-Guzzo_Leao.py
@@ -1,22 +1,12 @@
 from SPARQLWrapper import SPARQLWrapper, JSON
 from functools import reduce
 import time
-# Início da contagem de tempo
-#start_time_ = time.time()
 
-# Seu código a ser medido
-
-# Fim da contagem de tempo
-#end_time_ = time.time()
-
-
-
-# Impressão do tempo de execução
-#print(f"Tempo de execução: {end_time_-start_time_:.3f}} segundos")
 
 
 # Início da contagem de tempo
 start_time__query = time.time()
+
 #set endpoint
 sparql = SPARQLWrapper("http://dbpedia.org/sparql") 
 #set query
@@ -71,8 +61,6 @@ sparql.setReturnFormat(JSON)
 
 # execute query and returns result in JSON format
 results = sparql.query().convert() 
-
-#print(results)
 
 
 ### PROCESSING THE RESULTS USING FUNCTIONAL FEATURES
@@ -132,7 +120,6 @@ if results:
                      map ( lambda key: (key, item[key]['value']) 
                            , item.keys() )
              ) , VALUES)
-#print(list(res))
 
 def getRes(resDcit):
   VALUES = resDcit['results']['bindings']
@@ -144,6 +131,9 @@ def getRes(resDcit):
 
 
 
+##########################################
+##########################################
+##########################################
 ##########################################
 ##########################################
 ##########################################
@@ -244,21 +234,16 @@ def ano_publicados(lista):
 
 #retorna os resultados que possuem o ano de publicacao apos data
 def ano_public_apos_ano(lista, data):
-  ano = str(data)
-  return list( filter(lambda x: x['ano_publicado']>ano, lista) )
+  return list( filter(lambda x: int(x['ano_publicado'])>data, lista) )
 #retorna os resultados que possuem o ano de publicacao antes de data
 def ano_public_antes_ano(lista, data):
-  ano = str(data)
-  return list( filter(lambda x: x['ano_publicado']<ano, lista) )
+  return list( filter(lambda x: int(x['ano_publicado'])<data, lista) )
 #retorna os resultados que possuem o ano de publicacao igual a data
 def ano_public_mesmo_ano(lista, data):
-  ano = str(data)
-  return list( filter(lambda x: x['ano_publicado']==ano, lista) )
+  return list( filter(lambda x: int(x['ano_publicado'])==data, lista) )
 
 def ano_public_entre_data1_data2(lista, data1, data2):
-  ano1 = str(data1)
-  ano2 = str(data2)
-  return list( filter(lambda x: x['ano_publicado']>ano1 and x['ano_publicado']<ano2, lista) )
+  return list( filter(lambda x: int(x['ano_publicado'])>data1 and int(x['ano_publicado'])<data2, lista) )
 
 #retorna a lista de livros de um ano
 def ano_lista_livros(lista,data):
@@ -280,6 +265,10 @@ def ano_lista_maior_N(lista, n):
 #----------Sobre a publicadora
 #
 #
+#lista de autores de que possuem *autor* em alguma parte do seu nome
+def publicadora_filtra_nome(lista, publicadora):
+  return list (filter(lambda x: x['autor_'].__contains__(publicadora) , lista) )
+
 #retorna os resultados que possuem a publicadora
 def publicadora_igual(lista, publicadora):
   return list (filter(lambda x: x['publicado_por'] == publicadora , lista) )
@@ -296,10 +285,24 @@ def publicadora_lista_livros(lista,publicadora):
   return list(set([x['livro'] for x in lista_livros]))
 
 
+
 ##
 #----------Sobre o numero de paginas
 #
 #
+#retorna os resultados que possuem o mais paginas que pag
+def pagina_livro_mais_quantidade_pagina(lista, pag):
+  return list( filter(lambda x: int(x['pages']) > pag, lista) )
+#retorna os resultados que possuem o ano de publicacao antes de data
+def pagina_livro_menos_quantidade_pagina(lista, pag):
+  return list( filter(lambda x: int(x['pages']) < pag, lista) )
+#retorna os resultados que possuem o ano de publicacao igual a data
+def pagina_livro_mesma_quantidade_pagina(lista, pag):
+  return list( filter(lambda x: int(x['pages'])==pag, lista) )
+
+def ano_public_entre_data1_data2(lista, data1, data2):
+  return list( filter(lambda x: int(x['pages'])>data1 and int(x['pages'])<data2, lista) )
+
 #retorna uma lista de tuplas contendo (nome_do_livro, paginas)
 def pagina_lista_livros_com_pagina(lista):
   return list( set([ ( pos['livro'], pos['pages'] ) for pos in lista]))
@@ -307,6 +310,8 @@ def pagina_lista_livros_com_pagina(lista):
 #retorna uma lista de tuplas contendo (nome_do_livro, paginas), usando map
 def pagina_lista_livros_com_pagina_usandoMap(lista):
   return list( set(map( lambda pos: (pos['livro'], pos['pages']) ,lista)))
+
+
 #retorna uma lista de tuplas contendo (autor, nome_do_livro, paginas)
 def pagina_lista_livros_com_pagina_e_autor(lista):
   return list( set([ ( pos['autor_'],pos['livro'], pos['pages'] ) for pos in lista]))
@@ -335,12 +340,12 @@ def pagina_lista_livros_paginas_entre_N_M(lista, n, m):
 ----------------------
 """
 
-res = getRes(results)
+resultados_query = getRes(results)
 
 # Fim da contagem de tempo da query
 end_time__query = time.time()
 # Impressão do tempo de execução
-print(f"Tempo de execução da query: {end_time__query-start_time__query:.3f} segundos")
+#print(f"Tempo de execução da query: {end_time__query-start_time__query:.3f} segundos")
 
 
 
@@ -348,23 +353,26 @@ print(f"Tempo de execução da query: {end_time__query-start_time__query:.3f} se
 
 #testador de funcoes implementadas
 def meu_testador_1(res):
+
   autor_ex = "Terry Pratchett"
   ano_ex = 2007
   quantidade_ex = 50
+
   print("\nLista de livros de ", autor_ex, ":\n", autor_lista_livros(res,autor_ex))
   print("\nLista de livros do ano ",ano_ex, ":\n", ano_lista_livros(res, ano_ex))
   print("\nLista de anos com mais de ", quantidade_ex, " livros:\n", ano_lista_maior_N(res, quantidade_ex))
-  
+
 #testador de funcoes implementadas
 def meu_testador_2(res):
-  print("Metodo map e comprehension sao iguais para lista de livros com pagina? ", pagina_lista_livros_com_pagina_usandoMap(res) == pagina_lista_livros_com_pagina(res))
-  print(pagina_lista_livros_com_pagina_usandoMap(res))
+  print("Metodo map e comprehension sao iguais para lista de livros com pagina? ", set(pagina_lista_livros_com_pagina_usandoMap(res)) == set(pagina_lista_livros_com_pagina(res)))
 
 #testador de funcoes implementadas
 def meu_testador_3(res):
+
   autor_ex = "Mary Renault"
   autor_filtra = "Mary"
   publicadora_ex = "Del Rey Books"
+
   print("\nLista de livros da publicadora ", publicadora_ex, ":\n", publicadora_lista_livros(res,publicadora_ex))
   print("\nLista de publicadoras em contato com ", autor_ex, ":\n", autor_contato_com_publicadora(res,autor_ex))
   
@@ -373,23 +381,32 @@ def meu_testador_3(res):
 
 #combinando funcoes implementadas
 def meu_testador_4(res):
-  autor_filtra = "Mary"
+  autor_filtra = "John"
   ano_ex = 2015
-  lista_resultados = autor_filtra_nome(res, autor_filtra)
+  pagina_ex = 300
+
+  lista_autor = autor_filtra_nome(res, autor_filtra)
+  lista_autor_ano = ano_public_apos_ano(lista_autor, ano_ex)
+  lista_autor_ano_pagina =  pagina_livro_mais_quantidade_pagina(lista_autor_ano, pagina_ex)
+  
+  print("\nFiltrando resultados de nome do autor com ",autor_filtra, ":")
+  print(  lista_autor)
   print("\nFiltrando resultados de nome do autor com ",autor_filtra, "e apos o ano ", ano_ex, ":")
-  print_list_melhorado(ano_public_apos_ano(lista_resultados, ano_ex))
+  print_list_melhorado( lista_autor_ano)
+  print("\nFiltrando resultados de nome do autor com ",autor_filtra, ", apos o ano ", ano_ex, " e com mais paginas que ", pagina_ex, ":")
+  print_list_melhorado( lista_autor_ano_pagina)
 
 
 ##########################################
 ##########################################
 ##########################################
 ##########TESTES RECOMENDADOS#############
-#meu_testador_1(res)
-#meu_testador_2(res)
-meu_testador_3(res)
-meu_testador_4(res)
+#meu_testador_1(resultados_query)
+meu_testador_2(resultados_query)
+#meu_testador_3(resultados_query)
+#meu_testador_4(resultados_query)
 
-####FAZER COM OS 2 RESULTADOS FUNCOES QUE RETORNAM FILTRO DO DICIONARIO
-####Publicadora
-####Paginas
 
+
+
+#transformar alguns comprehension para map
